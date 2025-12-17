52763de9d4642eb5c9fb09cf6fe9eafcc5f17852
Onyekachukwu Aniakor <carlosaniakorchukwu@gmail.com>
2025-12-17 04:23:37 +0000

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({setQuery}) => {
     const {user,logout}= useAuth()
     
   return (
-    <nav className='bg-gray-900 p-4 text-white flex justify-between items-center w-full md:w-fit sm:w-fit ' >
+    <nav className='bg-gray-900 p-4 text-white flex justify-between items-center w-full  ' >
         <div className='text-xl font-bold'>
             <Link to='/'>NoteApp</Link>
         </div>
