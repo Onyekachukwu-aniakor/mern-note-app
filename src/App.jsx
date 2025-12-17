f0592799967b9d54c62af8dab49542801e5e1516
Onyekachukwu Aniakor <carlosaniakorchukwu@gmail.com>
2025-12-17 04:38:01 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { ToastContainer } from "react-toastify"
 function App() {
 
   return (
-    <div className="bg-gradient-to-br from-blue-200 to-purple-400" >
+    <div >
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
