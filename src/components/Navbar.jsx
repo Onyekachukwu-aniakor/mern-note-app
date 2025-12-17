e219cf20de2a612be335b731f16ab21815485926
Onyekachukwu Aniakor <carlosaniakorchukwu@gmail.com>
2025-12-17 13:44:44 +0000

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ const Navbar = ({setQuery}) => {
     const {user,logout}= useAuth()
     
   return (
-    <nav className='bg-gray-900 p-4 text-white flex justify-evenly items-center  rounded md:grid-cols-3 md:items-center md:grid sm:grid  md:justify-evenly '  >
-        <div className='text-xl font-bold'>
+    <nav className='bg-gray-900 p-4 text-white flex justify-between items-center  rounded md:grid-cols-3 md:items-center md:grid sm:grid  md:justify-evenly '  >
+        <div className='text-xl font-bold mr-2'>
             <Link to='/'>NoteApp</Link>
         </div>
         <input type="text" placeholder='Search notes...' className='bg-gray-600 py-2 px-4 rounded-lg mr-3 w-full' onChange={(e)=>setQuery(e.target.value)} /><div>
             
             {!user ? (
                 <>
-                <Link to='/login' className='bg-blue-500 py-2 px-4 rounded mr-4'>Login</Link>
-            <Link to='/register' className='bg-green-500 py-2 px-4 rounded mr-4'>Signup</Link>
+                <Link to='/login' className='bg-blue-500 py-2 px-4 rounded mr-3'>Login</Link>
+            <Link to='/register' className='bg-green-500 py-2 px-4 rounded mr-3'>Signup</Link>
                 </>
             )
             : <>
